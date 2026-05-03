5fafdd843a28dfdaeaef3e021bc3beefa5503326
karn <karnkumarpoddar0987@gmail.com>
2026-05-03 17:24:20 +0530

    final fullstack setup

--- scamshield-x/server.js
+++ scamshield-x/server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 const server = app.listen(PORT, () => {
   console.log(`\n  ==========================================`);
   console.log(`   ScamShield X is LIVE!`);
